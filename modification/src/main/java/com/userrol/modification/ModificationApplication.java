87f632825d43ca4db2884d58539b3ec0eec3a7e3
Henry <enriquenunez9996@hotmail.com>
2026-04-23 17:41:00 -0400

    Creación de archivo docker
    Se modifico el código main y se creo la imagen docker

--- modification/src/main/java/com/userrol/modification/ModificationApplication.java
+++ modification/src/main/java/com/userrol/modification/ModificationApplication.java
@@ -1,13 +1,30 @@
 package com.userrol.modification;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(
+        title       = "TechFix - User Microservice API",
+        version     = "1.1",
+        description = "Gestión de usuarios Techfix",
+        contact     = @Contact(
+            name  = "TechFix Admin",
+            email = "admin@techfix.cl"
+        )
+    ),
+    servers = {
+        @Server(url = "http://localhost:8080", description = "Local"),
+        @Server(url = "http://user-service:8080", description = "Docker")
+    }
+)
 public class ModificationApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ModificationApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ModificationApplication.class, args);
+    }
 }
