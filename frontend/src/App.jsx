a61feb60750ee798b98d89c0c724a7f463d3ccbc
Henry <enriquenunez9996@hotmail.com>
2026-05-01 17:18:01 -0400

    .
    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { Toaster } from 'react-hot-toast';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import ProtectedRoute from './components/ProtectedRoute';
-
 import Home from './pages/Home';
 import Catalogo from './pages/Catalogo';
 import Agendamiento from './pages/Agendamiento';
@@ -24,13 +23,10 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/catalogo" element={<Catalogo />} />
             <Route path="/login" element={<Login />} />
-
             <Route path="/agendamiento" element={<ProtectedRoute><Agendamiento /></ProtectedRoute>} />
             <Route path="/ordenes" element={<ProtectedRoute><Ordenes /></ProtectedRoute>} />
             <Route path="/pagos" element={<ProtectedRoute><Pagos /></ProtectedRoute>} />
-
             <Route path="/inventario" element={<ProtectedRoute allowedRoles={['TECNICO', 'ADMIN']}><Inventario /></ProtectedRoute>} />
-
             <Route path="/usuarios" element={<ProtectedRoute allowedRoles={['ADMIN']}><Usuarios /></ProtectedRoute>} />
             <Route path="/admin" element={<ProtectedRoute allowedRoles={['ADMIN']}><Admin /></ProtectedRoute>} />
           </Routes>
